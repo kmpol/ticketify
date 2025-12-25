62e726e2091cf86e98f10f12b9533d2739d140f7
Karol <malickikarol0@gmail.com>
2025-12-25 05:17:34 +0100

    refactored TicketService to increase readability

--- src/main/java/io/malicki/ticketify/domain/ticket/TicketService.java
+++ src/main/java/io/malicki/ticketify/domain/ticket/TicketService.java
@@ -21,17 +21,27 @@ public class TicketService {
     @Transactional
     public void processTicketCreation(TicketEvent ticketEvent) {
         String ticketId = ticketEvent.ticketId();
-        Optional<ProcessedTicketEntity> processedTicketOptional = processedTicketRepository.findByTicketId(ticketId);
-        if(processedTicketOptional.isPresent()) {
-            log.warn("❌Ticket already has been processed of given ticketId: {}, skipping", ticketId);
+
+        if (isTicketProcessed(ticketId)) {
             return;
         }
+
         log.info("⚙️Processing ticket of id: {}", ticketId);
         if(true) {
             throw new ExternalServiceUnavailableException("Testing retry");
         }
+
         ticketProducer.sendTicketEventToNotificationTopic(ticketEvent);
         markTicketCreationProcessed(ticketId);
+    }
+
+    private boolean isTicketProcessed(String ticketId) {
+        Optional<ProcessedTicketEntity> processedTicketOptional = processedTicketRepository.findByTicketId(ticketId);
+        if(processedTicketOptional.isPresent()) {
+            log.warn("❌Ticket already has been processed of given ticketId: {}, skipping", ticketId);
+            return true;
+        }
+        return false;
     }
 
     private void markTicketCreationProcessed(String ticketId) {
